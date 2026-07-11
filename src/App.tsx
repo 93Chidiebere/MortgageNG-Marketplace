90a352f53322b1630d14fa96c9a9dabe7c6ea9a0
93Chidiebere <vincent.christopher.189736@unn.edu.ng>
2026-07-11 13:39:22 +0100

    feat: add Privacy Policy and Terms of Service pages

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,8 @@ import LenderOnboarding from "./pages/LenderOnboarding";
 import LenderProfile from "./pages/LenderProfile";
 import Favorites from "./pages/Favorites";
 import Notifications from "./pages/Notifications";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -56,6 +58,8 @@ const App = () => (
                 <Route path="/notifications" element={<Notifications />} />
                 <Route path="/application/:id" element={<ApplicationDetails />} />
                 <Route path="/application/:id/certificate" element={<PreApprovalCertificate />} />
+                <Route path="/privacy" element={<Privacy />} />
+                <Route path="/terms" element={<Terms />} />
                 {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                 <Route path="*" element={<NotFound />} />
               </Routes>
